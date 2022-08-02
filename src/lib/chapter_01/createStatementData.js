5973e7437ac0271a2f7b272ee2d48bd0cee2d818
bluesky <luversof@hanmail.net>
2022-08-02 20:21:05 +0900

    1.8 다형성을 활용해 계산 코드 재구성하기

--- src/lib/chapter_01/createStatementData.js
+++ src/lib/chapter_01/createStatementData.js
@@ -7,46 +7,16 @@ export default function createStatementData(invoice, plays) {
 	return statementData;
 
 	function enrichPerformance(aPerformance) {
+		const caculator = createPerformanceCaculator(aPerformance, playFor(aPerformance));
 		const result = Object.assign({}, aPerformance); // 얕은 복사 수행
-		result.play = playFor(result);
-		result.amount = amountFor(result);
-		result.volumeCredits = volumeCreditsFor(result);
+		result.play = caculator.play;
+		result.amount = caculator.amount;
+		result.volumeCredits = caculator.volumeCredits;
 		return result;
 	}
 
 	function playFor(aPerformance) {
 		return plays[aPerformance.playID];
-	}
-
-	function amountFor(aPerformance) {
-		let result = 0;
-		switch (playFor(aPerformance).type) {
-			case 'tragedy': // 비극
-				result = 40000;
-				if (aPerformance.audience > 30) {
-					result += 1000 * (aPerformance.audience - 30);
-				}
-				break;
-			case 'comedy': // 희극
-				result = 30000;
-				if (aPerformance.audience > 20) {
-					result += 10000 + 500 * (aPerformance.audience - 20);
-				}
-				result += 300 * aPerformance.audience;
-				break;
-			default:
-				throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-		}
-		return result;
-	}
-
-	function volumeCreditsFor(perf) {
-		let result = 0;
-		result += Math.max(perf.audience - 30, 0);
-		// 희극 관객 5명마다 추가 포인트를 제공한다.
-		if ('comedy' === playFor(perf).type) result += Math.floor(perf.audience / 5);
-
-		return result;
 	}
 
 	//여기서부터 중첩 함수 시작
@@ -56,5 +26,55 @@ export default function createStatementData(invoice, plays) {
 
 	function totalVolumeCredits(data) {
 		return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+	}
+}
+
+class PerformanceCaculator {
+	constructor(aPerformance, aPlay) {
+		this.performance = aPerformance;
+		this.play = aPlay;
+	}
+
+	get amount() {
+		throw new Error(`서브클래스에서 처리하도록 설계되었습니다.`);
+	}
+	get volumeCredits() {
+		return Math.max(this.performance.audience - 30, 0);
+	}
+}
+
+function createPerformanceCaculator(aPerformance, aPlay) {
+	switch (aPlay.type) {
+		case 'tragedy':
+			return new TragedyCaculator(aPerformance, aPlay);
+		case 'comedy':
+			return new ComedyCaculator(aPerformance, aPlay);
+		default:
+			throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+	}
+}
+
+class TragedyCaculator extends PerformanceCaculator {
+	get amount() {
+		let result = 40000;
+		if (this.performance.audience > 30) {
+			result += 1000 * (this.performance.audience - 30);
+		}
+		return result;
+	}
+}
+
+class ComedyCaculator extends PerformanceCaculator {
+	get amount() {
+		let result = 30000;
+		if (this.performance.audience > 20) {
+			result += 10000 + 500 * (this.performance.audience - 20);
+		}
+		result += 300 * this.performance.audience;
+		return result;
+	}
+
+	get volumeCredits() {
+		return super.volumeCredits + Math.floor(this.performance.audience / 5);
 	}
 }
