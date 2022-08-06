befdb9263174d1702d8f3decbf3886bcd0fea936
bluesky <luversof@hanmail.net>
2022-08-07 00:46:52 +0900

    7.7 위임 숨기기

---
+++ src/lib/chapter_07/example_08.js
@@ -0,0 +1,39 @@
+// 7.7 위임 숨기기
+class Person {
+	constructor(name) {
+		this._name = name;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	get department() {
+		return this._department;
+	}
+
+	set department(arg) {
+		this._department = arg;
+	}
+}
+
+class Department {
+	get chargeCode() {
+		return this._chargeCode;
+	}
+
+	set chargeCode(arg) {
+		this._chargeCode = arg;
+	}
+
+	get manager() {
+		return this._manager;
+	}
+
+	set manager(arg) {
+		this._manager = arg;
+	}
+}
+
+// 클라이언트...
+manager = aPerson.department.manager;
