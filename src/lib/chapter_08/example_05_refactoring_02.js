baa543c57c3f085c25aa41a6780ff0de6816103f
bluesky <luversof@hanmail.net>
2022-08-08 22:20:07 +0900

    8.3 문장을 함수로 옮기기

---
+++ src/lib/chapter_08/example_05_refactoring_02.js
@@ -0,0 +1,19 @@
+// 8.3 문장을 함수로 옮기기
+function renderPerson(outStream, person) {
+	const result = [];
+	result.push(`<p>${person.name}</p>`);
+	result.push(renderPhoto(person.photo));
+	result.push(emitPhotoData(person.photo));
+	return result.join('\n');
+}
+
+function photoDiv(aPhoto) {
+	return ['<div>', emitPhotoData(aPhoto), '</div>'].join('\n');
+}
+function emitPhotoData(aPhoto) {
+	return [
+		`<p>제목: ${aPhoto.title}</p>`,
+		`<p>위치: ${aPhoto.location}</p>`,
+		`<p>날짜: ${aPhoto.date.toDateString()}</p>`
+	].join('\n');
+}
