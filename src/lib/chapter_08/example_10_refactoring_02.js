98abad7f1657d4987fb85be60636456169baf8bb
bluesky <luversof@hanmail.net>
2022-08-08 23:25:49 +0900

    8.8 반복문을 파이프라인으로 바꾸기

---
+++ src/lib/chapter_08/example_10_refactoring_02.js
@@ -0,0 +1,14 @@
+// 8.8 반복문을 파이프라인으로 바꾸기
+function acquireData(input) {
+	const lines = input.split('\n');
+	const result = [];
+	const loopItems = lines.slice(1);
+	for (const line of loopItems) {
+		if (line.trim() === '') continue;
+		const record = line.split(',');
+		if (record[1].trim() === 'India') {
+			result.push({ city: record[0].trim(), phone: record[2].trim });
+		}
+	}
+	return result;
+}
