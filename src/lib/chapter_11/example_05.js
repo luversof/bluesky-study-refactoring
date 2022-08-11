e3a43354192bd8420d7189c4eaa3eca60f1dae38
bluesky <luversof@hanmail.net>
2022-08-12 02:03:25 +0900

    11.3 플래그 인수 제거하기

---
+++ src/lib/chapter_11/example_05.js
@@ -0,0 +1,24 @@
+// 11.3 플래그 인수 제거하기
+
+// 클라이언트 1
+aShipment.deliveryDate = deliveryDate(anOrder, true);
+
+// 클라이언트 2
+aShipment.deliveryDate = deliveryDate(anOrder, false);
+
+function deliveryDate(anOrder, isRush) {
+	let result;
+	let deliveryTime;
+	if (anOrder.deliveryState === 'MA' || anOrder.deliveryState === 'CT')
+		deliveryTime = isRush ? 1 : 2;
+	else if (anOrder.deliveryState === 'NY' || anOrder.deliveryState === 'NH') {
+		deliveryTime = 2;
+		if (anOrder.deliveryState === 'NH' && !isRush) deliveryTime = 3;
+	} else if (isRush) deliveryTime = 3;
+	else if (anOrder.deliveryState === 'ME') deliveryTime = 3;
+	else deliveryTime = 4;
+
+	result = anOrder.placedOn.plusDays(2 + deliveryTime);
+	if (isRush) result = result.minusDays(1);
+	return result;
+}
