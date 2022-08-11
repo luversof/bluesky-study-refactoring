3be80b9e9e3ca8e9529e5e435efa08204fbb94ed
bluesky <luversof@hanmail.net>
2022-08-12 01:45:00 +0900

    11.1 질의 함수와 변경 함수 분리하기

---
+++ src/lib/chapter_11/example_01.js
@@ -0,0 +1,16 @@
+// 11.1 질의 함수와 변경 함수 분리하기
+function alertForMiscreant(people) {
+	for (const p of people) {
+		if (p === '조커') {
+			setOffAlarms();
+			return '조커';
+		}
+		if (p === '사루만') {
+			setOffAlarms();
+			return '사루만';
+		}
+	}
+	return '';
+}
+
+const found = alertForMiscreant(people);
---
+++ src/lib/chapter_11/example_01_refactoring_02.js
@@ -0,0 +1,30 @@
+// 11.1 질의 함수와 변경 함수 분리하기
+function alertForMiscreant(people) {
+	if (findMiscreant(people) != '') setOffAlarms();
+	for (const p of people) {
+		if (p === '조커') {
+			setOffAlarms();
+			return;
+		}
+		if (p === '사루만') {
+			setOffAlarms();
+			return;
+		}
+	}
+	return '';
+}
+
+function findMiscreant(people) {
+	for (const p of people) {
+		if (p === '조커') {
+			return '조커';
+		}
+		if (p === '사루만') {
+			return '사루만';
+		}
+	}
+	return '';
+}
+
+const found = findMiscreant(people);
+alertForMiscreant(people);
