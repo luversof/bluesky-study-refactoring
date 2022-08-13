697f137e31c2bd79e0a5111946370524f00f60e8
bluesky <luversof@hanmail.net>
2022-08-13 16:29:09 +0900

    12.8 슈퍼클래스 추출하기

---
+++ src/lib/chapter_12/example_07_refactoring_03.js
@@ -0,0 +1,58 @@
+// 12.8 슈퍼클래스 추출하기
+class Party {
+	constructor(name) {
+		this._name = name;
+	}
+
+	// 이름
+	get name() {
+		return this._name;
+	}
+}
+
+class Employee extends Party {
+	constructor(name, id, monthlyCost) {
+		super(name);
+		this._id = id;
+		this._monthlyCost = monthlyCost;
+	}
+
+	//월간 비용
+	get monthlyCost() {
+		return this._monthlyCost;
+	}
+
+	get id() {
+		return this._id;
+	}
+
+	// 연간 비용
+	get annualCost() {
+		return this._monthlyCost * 12;
+	}
+}
+
+class Department extends Party {
+	constructor(name, staff) {
+		super(name);
+		this._staff = staff;
+	}
+
+	get staff() {
+		return this._staff.slice();
+	}
+
+	// 총 월간 비용
+	get totalMonthlyCost() {
+		return this.staff.map((e) => e.monthlyCost).reduce((sum, cost) => sum + cost);
+	}
+
+	get headCount() {
+		return this.staff.length;
+	}
+
+	// 총 연간 비용
+	get totalAnnualCost() {
+		return this.totalMonthlyCost * 12;
+	}
+}
